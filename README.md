c261bb8e301de34aaf9f9909c8b843d3308bc010
ashwinhubli <66745007+ashwinhubli@users.noreply.github.com>
2020-06-29 18:02:44 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# project
+# staticSquare
+A static square made using Matter.js physics engine
---
+++ wall.js
@@ -0,0 +1,25 @@
+class DustbinWall{
+    constructor(x,y,width,height)
+    {
+     var options={
+         'isStatic':true
+     }
+     this.image = loadImage("sprites/dustbinpapergreen.png");
+         this.body = Bodies.rectangle(x,y,width,height,options);
+         this.width = width;
+         this.height = height;
+         
+         World.add(world,this.body);
+    }
+  
+  
+  display(){
+     push();
+      fill('blue');
+      rect(this.body.position.x,this.body.position.y,this.width,this.height);
+      
+     pop();
+  }
+  
+  }
+  
